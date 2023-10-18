00e128fbc6654ee22e9c93adc90143ca982ef2fa
Josh Yamamoto <joshuayamamoto5@gmail.com>
2023-10-18 11:12:46 -0500

    bug fix

--- R/whichFIESTA.R
+++ R/whichFIESTA.R
@@ -13,7 +13,7 @@ whichFIESTA <- function(func_name) {
   out <- full_funcs_df[full_funcs_df[["function_name"]] == func_name, , drop = FALSE]
 
 
-  if (length(out) == 0) {
+  if (nrow(out) == 0) {
     message(paste0("Could not find ", func_name, "in either FIESTA or FIESTAutils"))
     return(NULL)
   } else {
